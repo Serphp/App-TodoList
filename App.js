a12e85d0d7e1401601c80253ba2e129f8067de44
Bryan Rodriguez <Serphp@hotmail.com>
2023-06-07 02:27:09 -0500

    [Update] New dependencies & navigations were implemented

--- App.js
+++ App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry, Layout, Text } from '@ui-kitten/components';
+
+import * as React from 'react';
+import { ApplicationProvider, IconRegistry, Layout } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import * as eva from '@eva-design/eva';
 import TodoList from './src/components/TodoList';
-import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import NewScreen from './Screen/newScreen';
+import { View, Text, Button } from 'react-native';
 
-const HomeScreen = () => (
-  <Layout style={{flex: 1}}>
-    <SafeAreaView>
-      <TodoList/>
-    </SafeAreaView>
-  </Layout>
-);
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
 
-export default () => (
-  <>
-  <IconRegistry icons={EvaIconsPack} />
-  <ApplicationProvider {...eva} theme={eva.light}>
-  <HomeScreen />
-  </ApplicationProvider>
-  </>
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> Pagina 2 </Text>
+      <Button title="Go to newScreen" onPress={() => navigation.navigate('newScreen')} />
+      <Button title="Go to TodoList" onPress={() => navigation.navigate('TodoList')} />
+    </View>
+  );
+}
 
-);
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <>
+<IconRegistry icons={EvaIconsPack} />
+<ApplicationProvider {...eva} theme={eva.light}>
+  <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Screen name="newScreen" component={NewScreen} />
+      <Stack.Screen name="TodoList" component={TodoList} />
+    </Stack.Navigator>
+  </NavigationContainer>
+</ApplicationProvider>
+    </>
+
+  );
+}
+
+export default App;
